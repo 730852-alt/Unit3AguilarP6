02c409732e22185a66535d4f1c380c81f1281648
730852-alt <730852@student.dusd.net>
2026-03-25 13:47:13 -0700

    hi

--- P6AguilarUnit3/Assets/Scripts/PlayerController.cs
+++ P6AguilarUnit3/Assets/Scripts/PlayerController.cs
@@ -17,7 +17,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) && isOnGround)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
         {
             playerRB.AddForce(Vector3.up * 10, ForceMode.Impulse);
             isOnGround = false;
