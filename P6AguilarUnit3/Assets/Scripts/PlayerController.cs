fe2c739a380cf1f3083681d93c8926af9e44c4f6
730852-alt <730852@student.dusd.net>
2026-04-02 13:46:58 -0700

    3.4 or something

--- P6AguilarUnit3/Assets/Scripts/PlayerController.cs
+++ P6AguilarUnit3/Assets/Scripts/PlayerController.cs
@@ -9,34 +9,44 @@ public class PlayerController : MonoBehaviour
     public float gravityModifier;
     public bool isOnGround = true;
     public bool gameOver;
+    private Animator playerAnim;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         playerRB = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             playerRB.AddForce(Vector3.up * 10, ForceMode.Impulse);
             isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig"); 
+            dirtParticle.Stop();
         }
     }
     private void OnCollisionEnter(Collision collision)
     {
         isOnGround = true;
-
+        dirtParticle.Play();
         if (collision.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
-        } else if (collision.gameObject.CompareTag("Obastacle"))
+        } else if (collision.gameObject.CompareTag("Obstacle"))
         {
             Debug.Log("Game Over");
             gameOver = true;
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
+            explosionParticle.Play();
+            dirtParticle.Stop();
         }
         
     }
